94e1d8bb60e64b613151fdcab293073f238b5508
Cris Mihalache <cris.mihalache@spectrumit.eu>
2015-03-20 15:55:44 +0100

    Add RVM functions, bump ruby version

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 set fish_path $HOME/.oh-my-fish
-set PATH /home/cris/.gem/ruby/2.2.0/bin $PATH
-rvm use 2.2.0 --default
+set PATH /home/cris/.gem/ruby/2.2.1/bin $PATH
+rvm use 2.2.1 --default
 
 . $fish_path/oh-my-fish.fish
