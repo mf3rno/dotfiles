fefde9fabefe31f3852e802f985dce3d6dbd997b
Cris Mihalache <cris.mihalache@spectrumit.eu>
2015-10-14 15:46:24 +0200

    Updated some fish settings, better ncmpcpp visual config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_path $HOME/.oh-my-fish
-set PATH /home/cris/.gem/ruby/2.2.1/bin $PATH
+set PATH /usr/local/bin /home/cris/.gem/ruby/2.2.1/bin ~/Android/Sdk/platform-tools/ $PATH
+set ANDROID_HOME /home/cris/Android/Sdk
+set LC_ALL en_US.UTF-8
 rvm use 2.2.1 --default
 
 . $fish_path/oh-my-fish.fish
