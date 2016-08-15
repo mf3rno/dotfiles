5caa942d46bf61a86b1fc095ffa1767b4b7af672
Cris Mihalache <me@f3rno.com>
2016-08-15 13:17:33 +0200

    migration to new XPS laptop, kill unused stuff

--- config/awesome/READ_THIS.md
+++
@@ -1,4 +0,0 @@
-I killed off the repo info for both this and the lain/ sub-folder to
-make dotfile management simpler.
-
-Originally copied and modified from: https://github.com/copycat-killer/awesome-copycats
--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,3 +1,11 @@
 function fish_greeting
-  fortune -o 
+  task
+
+  set UPDATES_AVAILABLE (cat /home/f3rno/data/update-count)
+
+  if math "$UPDATES_AVAILABLE > 0" > /dev/null
+    set_color blue
+    printf "\n$UPDATES_AVAILABLE updates available via dnf"
+    set_color normal
+  end
 end
