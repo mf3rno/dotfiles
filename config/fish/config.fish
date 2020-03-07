69e25b6e34034cf934293bf040fe3e9e2e16424f
Cris Mihalache <me@f3rno.com>
2020-03-07 11:22:48 +0700

    (refactor) alias cat to bat in fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,7 @@ set -x PATH $PATH ~/bin ~/.local/bin ~/.rvm/rubies/default/bin
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore node_modules --ignore bower_components --ignore dist --ignore build --ignore db -g ""'
 
 alias vim='nvim'
+alias cat='bat'
 
 # nvm use 12.16.1 --silent
 # rvm use 2.7.0 --silent > /dev/null
